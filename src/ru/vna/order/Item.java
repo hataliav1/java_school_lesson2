78dbaf404960e04dcc99fcabb9d7eb035e87873c
hataliav1 <40887268+hataliav1@users.noreply.github.com>
2018-07-06 12:45:52 +0300

    Add Item

---
+++ src/ru/vna/order/Item.java
@@ -0,0 +1,18 @@
+package ru.vna.order;
+
+/**
+ * Created by Voronina2-na on 06.07.2018.
+ */
+public final class Item {
+
+    private final String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Item (String name){
+        this.name = name;
+    }
+
+}
